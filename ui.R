1e4d1c12547f3265d4723ec536434c14f93002c4
Mendrika627 <mimimeme861@gmail.com>
2025-10-22 12:26:22 +0300

    Version avec 3 indicateurs développés interface responsive

--- ui.R
+++ ui.R
@@ -12,39 +12,39 @@ ui <- bootstrapPage(
     ),
     id = "main_nav",
     tabPanel("Cartographie", icon = shiny::icon("map"),
-       div(class="outer",
-        tags$head(includeCSS("./styles.css")),
-        leafletOutput("map_placeholder", width="100%", height="100%"),
-        absolutePanel(id = "controls", class = "panel panel-primary",
-                      style = " border: none;box-shadow: none;
+             div(class="outer",
+                 tags$head(includeCSS("./styles.css")),
+                 leafletOutput("map_placeholder", width="100%", height="100%"),
+                 absolutePanel(id = "controls", class = "panel panel-primary",
+                               style = " border: none;box-shadow: none;
     transition: none;
     pointer-events: auto;",
-                      class = "panel panel-primary",
-                      top = 150, left = 50, width = '50%', fixed=TRUE,draggable = TRUE,
-                      div(class = "main-content",
-                          h3(textOutput("indicateurs")),
-                          h4(textOutput("text_village_selected")),
-                          shinycssloaders::withSpinner(
-                            plotlyOutput("result_map_click", height = "600px"),
-                            type = 1,       # (optionnel) type de spinner
-                            color = "#2596be",
-                            color.background = "white"
-                          ),
-                          p(textOutput("legende"))
-                      )
-        ),
-        absolutePanel(id = "controls", class = "panel panel-primary",
-          top = 80, right = 50, width = '30%', fixed=TRUE,
-          fluidRow(
-            column(12, linebreaks(1),
-                   div(class = "parametres-container",
-                       shinyWidgets::pickerInput("indicateurs","Indicateurs à visualiser",choices = c("Démographie de la communauté des pêcheurs","Proportions de pêcheurs enquêtés par filières halieutiques","Proportions de pêcheurs enquêtés par techniques de pêches"),multiple = FALSE,selected = "Démographie de la communauté des pêcheurs",width = '100%')
-                   )
-            )
-          )
-        ),
-        
-      )
+                               class = "panel panel-primary",
+                               top = 100, left = 50,height = "50%", width = '50%', fixed=TRUE,draggable = TRUE,
+                               div(class = "main-content",
+                                   h3(textOutput("indicateurs")),
+                                   h4(textOutput("text_village_selected")),
+                                   shinycssloaders::withSpinner(
+                                     plotlyOutput("result_map_click", height = "80%"),
+                                     type = 1,       # (optionnel) type de spinner
+                                     color = "#2596be",
+                                     color.background = "white"
+                                   ),
+                                   p(textOutput("legende"))
+                               )
+                 ),
+                 absolutePanel(id = "controls", class = "panel panel-primary",
+                               top = 80, right = 50, width = '30%', fixed=TRUE,
+                               fluidRow(
+                                 column(12, linebreaks(1),
+                                        div(class = "parametres-container",
+                                            shinyWidgets::pickerInput("indicateurs","Indicateurs à visualiser",choices = c("Démographie de la communauté des pêcheurs","Proportions de pêcheurs enquêtés par filières halieutiques","Proportions de pêcheurs enquêtés par techniques de pêches"),multiple = FALSE,selected = "Démographie de la communauté des pêcheurs",width = '100%')
+                                        )
+                                 )
+                               )
+                 ),
+                 
+             )
     )
   )
 )
